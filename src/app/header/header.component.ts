7dfe370a2795bb939195457ba4f06a032c047bb1
Deepak Shrestha <deepak.shrestha@sbsolutionsnepal.com>
2023-11-20 19:21:59 +0545

    responsive side bar work done

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  @Input() sidebarActive = true;
+  @Output() toggleSidebar = new EventEmitter ();
+
+
+}
---
+++ src/app/login-form/login-form.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent {
+  loginForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      // Perform login logic here
+      console.log('Login successful!');
+    }
+  }
+}
